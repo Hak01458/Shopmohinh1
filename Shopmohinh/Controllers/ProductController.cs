1ec7f35ac8f0fe7d72c07c37b59af74096307633
admin <admin@LAPTOP-32BPF78Q>
2024-10-10 08:14:56 +0700

    allShopmohinh

---
+++ Shopmohinh/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shopmohinh.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GiohangDathang()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail()
+        {
+            return View();
+        }
+        public ActionResult asd()
+        {
+            return View();
+        }
+    }
+}
